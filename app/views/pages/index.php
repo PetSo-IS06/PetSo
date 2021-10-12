8904b0107ec627d4f2c410a4cff548fcd8e179dd
azeimtiaz <imtiaz.azma@gmail.com>
2021-10-12 13:03:00 +0530

    Hero banner section complete

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URL_ROOT; ?>/public/assets/CSS/global_custom.css">
+    <link rel="stylesheet" href="<?php echo URL_ROOT; ?>/public/assets/CSS/hero-section.css">
     
     <title><?php echo SITE_NAME; ?></title>
 
@@ -17,9 +18,14 @@
     </div>
     <div class="hero">
         <div class="hero-banner">
-            <div class="hero-banner-icon">
-                <h1>Hero Section</h1>
+            <div class="hero-banner-contents">
+                <div class="hero-banner-contents-icon">
+                    <img src="<?php echo URL_ROOT; ?>/public/assets/img/icons/paw-with-plus.png" alt="">
+                </div>
+                <h1>Report an Animal to an<br />Animal Shelter</h1>
             </div>
+        </div>
+        <div class="hero-cards">
         </div>
     </div>
 </body>

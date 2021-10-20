0636187040465a1d7fd0891838a1819459bc85fc
Saadique <48992310+Saadique@users.noreply.github.com>
2021-10-20 12:04:35 +0530

    organization_sign up added (#40)

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,15 @@
 <?php
 
-    //Site Info
-    define('SITE_NAME', 'PetSo');
+//Site Info
+define('SITE_NAME', 'PetSo');
 
-    //App Root (Dynamic Links)
-    define('APP_ROOT', dirname(dirname(__FILE__)));
-    define('URL_ROOT', 'http://localhost/PetSo');
-    define('URL_SUBFOLDER', 'PetSo');
+//App Root (Dynamic Links)
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', 'http://localhost/PetSo');
+define('URL_SUBFOLDER', 'PetSo');
 
-    //Database Info
-    define('DB_HOST', '52.74.232.121:3306');
-    define('DB_USER', 'petsodev');
-    define('DB_PASS', "Azma123&*^");
-    define('DB_NAME', 'petso');
-
+//Database Info
+define('DB_HOST', '52.74.232.121:3306');
+define('DB_USER', 'petsodev');
+define('DB_PASS', "Azma123&*^");
+define('DB_NAME', 'petso');

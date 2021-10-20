ebcdde3583688bc334fa76a1916cb4c257fbfb74
Saadique <48992310+Saadique@users.noreply.github.com>
2021-10-20 11:48:12 +0530

    All Welfare Project Top Section added (#30)

---
+++ app/views/pages/projectTopSection.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welfare Projects</title>
+    <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/project_top_section.css'>
+</head>
+
+<body>
+    <div class="header">
+        <h2>Welfare Projects</h2>
+        <p> Help animal welfare organizations make a difference <br>
+            through your contribution</p>
+
+        <div class="search-container">
+            <input type="text" placeholder="Search Welfare Project" name="search">
+        </div>
+
+    </div>
+
+    <div class="row" style="margin-top: 3%;">
+        <center>
+            <div class="column">
+                <h1>110</h1>
+                Organizations
+
+            </div>
+            <div class="column">
+                <h1>500</h1>
+                Projects
+            </div>
+            <div class="column">
+                <h1>220 </h1>
+                Ongoing
+            </div>
+            <div class="column">
+                <h1> 160</h1>
+                Completed
+            </div>
+        </center>
+    </div>
+
+
+    </div>
+</body>
+
+</html>

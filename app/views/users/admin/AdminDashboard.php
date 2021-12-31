c14d09ef0254b0e8fed727551c8752cde1d5be73
azeimtiaz <imtiaz.azma@gmail.com>
2021-12-31 23:09:08 +0530

    Dash side nav

--- app/views/users/admin/AdminDashboard.php
+++ app/views/users/admin/AdminDashboard.php
@@ -37,39 +37,45 @@
                         </a>
                     </li>
                     <li>
-                        <a class="active-tag" onClick="showProjectsPanel()" id="proj-tag">
+                        <a onClick="" id="">
                         <i class="fas fa-rocket"></i>
-                        <span>My Projects</span>
+                        <span>Users</span>
                         </a>
                     </li>
                     <li>
-                        <a onClick="showVolunteerPanel()" id="vol-tag">
+                        <a onClick="" id="">
                         <i class="fas fa-hands-helping"></i>
-                        <span>Volunteers</span>
+                        <span>Account Requests</span>
                         </a>
                     </li>
                     <li>
-                        <a onClick="showFaundraisingPanel()" id="fund-tag">
+                        <a class="active-tag" onClick="showProjectsPanel()" id="proj-tag">
                         <i class="fas fa-comment-dollar"></i>
-                        <span>Fundraisers</span>
+                        <span>Welfare Projects</span>
                         </a>
                     </li>
                     <li>
                         <a href="#0">
                         <i class="fas fa-paw"></i>
-                        <span>My Animals</span>
+                        <span>Animal Reports</span>
                         </a>
                     </li>
                     <li>
                         <a href="#0">
                         <i class="fas fa-dog"></i>
-                        <span>Adoptions</span>
+                        <span>Complaints</span>
                         </a>
                     </li>
                     <li>
                         <a href="#0">
                         <i class="fas fa-receipt"></i>
-                        <span>Sponsorships</span>
+                        <span>Finance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                        <i class="fas fa-receipt"></i>
+                        <span>My Account</span>
                         </a>
                     </li>
             </div>
@@ -106,8 +112,8 @@
                                     <th>Cause</th>
                                     <th>Create Date</th>
                                     <th>Organization</th>
-                                    <th>District</th>
-                                    <th>Area</th>
+                                    <th>Volunteering</th>
+                                    <th>Fundraising</th>
                                     <th>Action</th>
                                 </tr>
                                 </thead>
@@ -120,8 +126,8 @@
                                             <th><?php echo $item->cause; ?></th>
                                             <th><?php echo $item->create_date; ?></th>
                                             <th><?php echo $item->org_id; ?></th>
-                                            <th>District</th>
-                                            <th>Area</th>
+                                            <th><input type="checkbox" name=""></th>
+                                            <th><input type="checkbox" name=""></th>
                                             <th>Action</th>
                                         </tr>
                                     <?php } ?>

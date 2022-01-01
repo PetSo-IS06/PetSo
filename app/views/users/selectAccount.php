99fecf34f176895145eafb6a576656027d14f9d0
Amana Samsudeen <2019is006@stu.ucsc.lk>
2022-01-01 19:50:58 +0530

    colorChanged

--- app/views/users/selectAccount.php
+++ app/views/users/selectAccount.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title><?php echo SITE_NAME; ?> | Sign Up</title>  </head>
+<title> <?php echo SITE_NAME; ?> Select Account</title>  </head>
 <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/selectAccount.css'/>
 <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/components/step.css'/> 
 </center>
@@ -26,7 +26,7 @@
     </div>
 </center>
 <a href="<?php echo URL_ROOT; ?>/users/organizationSignUp">
-  <div class="select-left"> <center>
+  <div class="left"> <center>
     <h3> Welfare Organization </h3> <br>
       <img src="<?php echo URL_ROOT; ?>/public/assets/img/icons/organization.png" height="75px" width="75px">
 </center>
@@ -34,7 +34,7 @@
 </a>
 
   <a href="<?php echo URL_ROOT; ?>/users/signup">
-    <div class="select-right">  <center>
+    <div class="right">  <center>
       <h3> User  </h3> <br>  
       <img src="<?php echo URL_ROOT; ?>/public/assets/img/icons/user.svg" height="75px" width="75px"> 
 </center>

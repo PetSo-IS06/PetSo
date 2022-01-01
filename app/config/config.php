1c4d410cdbb0cc5c4e83fcbd42890116ec97c4fb
Amana Samsudeen <2019is006@stu.ucsc.lk>
2022-01-01 20:42:57 +0530

    enter the commit message for your changes. Lines starting

--- app/config/config.php
+++ app/config/config.php
@@ -1,21 +1,17 @@
 <?php
 
-    //Site Info
-    define('SITE_NAME', 'PetSo');
+//Site Info
+define('SITE_NAME', 'PetSo');
 
-    //App Root (Dynamic Links)
-    define('APP_ROOT', dirname(dirname(__FILE__)));
-    define('URL_ROOT', 'http://localhost/PetSo');
-    define('URL_SUBFOLDER', 'PetSo');
+//App Root (Dynamic Links)
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', 'http://localhost/PetSo');
+define('URL_SUBFOLDER', 'PetSo');
 
-    //Database Info
-
-    // define('DB_HOST', '52.74.232.121:3306');
-    // define('DB_USER', 'petsodev');
-    // define('DB_PASS', "Azma123&*^");
-
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_NAME', 'petso');
+//Database Info
+define('DB_HOST', '54.151.148.143:3306');
+define('DB_USER', 'petsodev');
+define('DB_PASS', "Azma123&*^");
+define('DB_NAME', 'petso');
+   
 

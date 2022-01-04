122e508f0a0ec1d469d3304578e86e75f3ff384c
Amana Samsudeen <2019is006@stu.ucsc.lk>
2022-01-04 22:08:17 +0530

    heading changed

--- app/views/users/organization/ProjectView.php
+++ app/views/users/organization/ProjectView.php
@@ -7,19 +7,22 @@
     
     <title>Welfare Project Header</title>
     <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/ProjectView.css'/>
+    <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/global_custom.css'/>
 </head>
 <body>
     <div class="row">
         <div class="column" style="background-color:white;"> <br>
             <center> <br>
-         <h2>Looking For Funds</h2> <br><br>
+         <div class="heading2B purple "> Looking For Funds </div> <br><br>
        <P> <b>150LKR </b> raised of 500K </P> <br><br>
+       
        <div class="slidecontainer" >
         <input type="range" min="1" max="500" value="150" >
                </div>      
          <br>
         <p>Check donation section below for more details </p> <br> <br> 
-         <button type="submit" class="btn"> Donate Now </button>
+         <!-- <button type="submit" class="-btn"> Donate Now </button> -->
+         <a href= "" class="purple-btn">donate now </a>
         </center>
         </div>
         <div class="column" style="background-color :white;">
@@ -27,8 +30,8 @@
         </div> <br><br>
         <div class="column" style="background-color:white;"> 
             <center>
-            <h2>Volunteers Needed</h2> <br>
-            <h1>20</h1> <br>
+            <div class="heading2"> Volunteers Needed  </div> <br>
+          <div class="heading1" > 20 </div> <br>
             Vacancies Left <br>
            <p> Check volunteer section below for more details </p> <br>
             <p>30 have applied already </p> <br> <br>
@@ -36,15 +39,12 @@
         </center>
         </div>
         </div>
-           
-    <center>
-        <div class= "Project_box">
-            <div class ="project">
+        
             <img src="<?php echo URL_ROOT; ?>/public/assets/img/welfare_img/image 3.png" width="100px" height="100px" border-radius="50px"><br>
-            <b> Project Title : </b>  <?php echo $data->title?><br> <br>
-            <b> Initiation date : </b> <?php echo $data->initiation_date?><br><br>
-            <b> Description : </b> <?php echo $data->description?> <br>
+           Sterilizing 50 Stray dogs <br>
+           Charley's Home by "Save a Paw" 
+           16 days 14 hours Left <br> 
+           for Project Initiation
 </div>
-</div>
-</center>
+
 </body>

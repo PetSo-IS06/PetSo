a84c579e3d43f825e7e156cb6e895eba8720ee81
Amana Samsudeen <2019is006@stu.ucsc.lk>
2022-03-22 12:42:00 +0530

    single project retriwed

--- app/views/projects/viewSingleProject.php
+++ app/views/projects/viewSingleProject.php
@@ -9,13 +9,15 @@
     <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/ProjectView.css'/>
     <link rel='stylesheet' href='<?php echo URL_ROOT; ?>/public/assets/CSS/global_custom.css'/>
 </head>
+
 <body>
-<div id="top-nav">
+        <div id="top-nav">
             <?php
             require APP_ROOT . '/views/components/topNavbar.php';
             ?>
         </div>
-        <div id="body" class="pg-body">
+
+<div id="body" class="pg-body">
     <div class="flexcontainer">
         <div class="columns"> <br>
             <center> <br>
@@ -24,7 +26,7 @@
          <div>
 
         <Input class="range" type="range" name "" value="150" min="150" max="500" onChange="rangeSlide(this.value)" onmousemove="rangeSlide(this.value)"></Input>
-    </div>
+       </div>
      
     <script type="text/javascript">
         function rangeSlide(value) {
@@ -42,7 +44,7 @@
         <img src="<?php echo URL_ROOT; ?>/public/assets/img/welfare_img/strayDog.png" width="100%" height="350px">
         </div> 
         <div class="columns"> <center> <br><br>
-            <div class="heading2B purple"> Volunteers Needed  </div>
+          <div class="heading2B purple"> Volunteers Needed  </div>
           <div class="title grey"> 20 </div>
           <div class="subtitleB grey">Vacancies Left  </div>
           <div class="normal grey">Check volunteer section below for more details <br><br>
@@ -53,30 +55,33 @@
         </div> <br>
         <div class="container">
         <div class="col1">
-            <img src="<?php echo URL_ROOT; ?>/public/assets/img/welfare_img/image 3.png" width="100px" height="100px" border-radius="50px"> </div>
+            <img src="<?php echo URL_ROOT; ?>/public/assets/img/welfare_img/image 3.png" width="100px" height="100px" border-radius="50px">
+          </div>
             <div class="col2">
             <div class="subtitleB grey"> Sterilizing 50 Stray dogs </div> 
-          <div class="normal grey"> Charley's Home by "Save a Paw" </div> </div>
+          <div class="normal grey"> Charley's Home by "Save a Paw" </div> 
+           </div>
           <div class="col3">
           <div class="heading1B grey" >16 days 14 hours Left </div> <br> 
            <div class="heading2 grey">for Project Initiation </div> 
           </div>
        </div>
-      </div>
+      <br>
 
       <!-- story -->
    
-<div id="body" class="pg-body">
+
     <div class="subtitleB grey"> Story </div> 
-    <hr>
+    <hr> <br>
 
     <div class="container">
         <div class="col1">
-        <div class="normalB"> Title: </div> <?php echo $data['project']->title ?> 
-        <div class="normalB max_height"> Description:</div> <?php echo $data['project']->description ?> 
-        <div class="normalB"> Cause: </div> <?php echo $data['project']->cause ?> 
-        <div class="normalB"> Initiation Date: </div> <?php echo $data['project']->initiation_date ?>
-        <div class="normalB"> Run by: </div> <?php echo $data['project']->org_name ?>
+        <div class="normalB"> Title: </div> <div class='normal'> <?php echo $data['project']->title ?>  </div>
+        <br>
+        <div class="normalB x"> Description:</div> <div class='normal'> <?php echo $data['project']->description ?> </div> <br>
+        <div class="normalB"> Cause: </div> <div class='normal'> <?php echo $data['project']->cause ?> </div> <br>
+        <div class="normalB"> Initiation Date: </div> <div class='normal'> <?php echo $data['project']->initiation_date ?> </div><br>
+        <div class="normalB"> Run by: </div><div class='normal'> <?php echo $data['project']->org_name ?> </div><br>
         </div>
 
             <div class="col2">     
@@ -92,6 +97,7 @@
     
     <!-- volunteering -->
 
+
     <?php
     if($data['project']->volunteering=="True"){
         $volunteer=$data['volunteer_opportunity'];
@@ -99,27 +105,27 @@
         echo" 
             
     <div class='subtitleB grey'> About Volunteering </div> 
-    <hr>
+    <hr><br>
     <div class='container'>
     <div class='col1'>
-        <div class='normalB max_height'> Description: </div> $volunteer->description 
-        <div class='normalB'> Application opening Date: </div>  $volunteer->app_open 
-        <div class='normalB'> Application Closing Date: </div>  $volunteer->app_close 
-        <div class='normalB'> <u> Work </u> </div> 
-        <div class='normalB'> Location :     $volunteer->district  </div>
-        <div class='normalB'> Work From:    $volunteer->work_from </div>
-        <div class='normalB'> Work End:    $volunteer->work_end  </div>
-        <div class='normalB'> Work Days:     $volunteer->days  </div>
-        <div class='normalB'> Start Date:     $volunteer->work_start </div>
-        <div class='normalB'> End Date:    $volunteer->work_end  </div>
-        <div class='normalB'> <u> Requirements </u> </div> 
-        $volunteer->requirements 
-        <div class='normalB'> <u> Additional Details </u> </div> 
+        <div class='normalB max_height'> Description: </div> <div class='normal'> $volunteer->description  </div> <br>
+        <div class='normalB'> Application opening Date: </div> <div class='normal'>  $volunteer->app_open  </div><br>
+        <div class='normalB'> Application Closing Date:  </div> <div class='normal'> $volunteer->app_close </div> <br>
+        <div class='normalB'> <u> Work </u> </div> <br>
+        <div class='normalB'> Location :     $volunteer->district  </div><br>
+        <div class='normalB'> Work From:    $volunteer->work_from </div> <br>
+        <div class='normalB'> Work End:    $volunteer->work_end  </div> <br>
+        <div class='normalB'> Work Days:     $volunteer->days  </div> <br>
+        <div class='normalB'> Start Date:     $volunteer->work_start </div> <br>
+        <div class='normalB'> End Date:    $volunteer->work_end  </div><br>
+        <div class='normalB'> <u> * Requirements </u> </div> 
+        $volunteer->requirements <br> <br>
+        <div class='normalB'> <u> *Additional Details </u> </div> 
         $volunteer->add_note 
     </div> 
 
       <div class='col2'>
-            <img src='$img_url' width='350px' height='200px' border-radius='20px'>
+            <img src='$img_url' width='350px' height='200px' border-radius='20px'> <br>
 
       <div class='container'>
       <div class='col1'>
@@ -130,32 +136,32 @@
         <div class='subtitleB grey'> People Applied </div>
         <div class='subtitleB grey'> 100 </div> 
       </div> 
-    </div> <br><br><center>
+    </div> <br> <center>
        <a href= '' class='purple-btn v-btn_width'>volunteer Now </a> </center>
    </div>
 </div> "; }?>
-</div>
+
 
        <br> 
 
        <!-- fundraising -->
-       <div id="body" class="pg-body">
+     
        <?php
        if($data['project']->fundraising=="True"){
            $fundraiser= $data['fundraiser'];
            echo"
        <div class='containter'>
         <div class='colum1'>
-            <div class='subtitleB grey'> About the funds </div> <hr>   
-            <div class='normalB max_height'> Description: </div> $fundraiser->funds_for
-            <div class='normalB'> Fundraising start date: </div> $fundraiser->funding_start 
-            <div class='normalB'> Fundraising end date: </div> $fundraiser->funding_end 
-        <div> <br>
-        <div class='container'>
+            <div class='subtitleB grey'> About the funds </div> <hr>  <br> 
+            <div class='normalB max_height'> Description: </div> <div class='normal'> $fundraiser->funds_for  </div><br>
+            <div class='normalB'> Fundraising start date:  $fundraiser->funding_start  </div><br>
+            <div class='normalB'> Fundraising end date:  $fundraiser->funding_end </div><br>
+        </div> <br>
+
+        <div class='flexcontainer'>
         <div class='donate_col1'> 
         <div class='normalB'> Total Needed </div>
         <div class='normalB'> Rs 100000 </div>
-        
         </div>
         
         <div class='donate_col2'> 
@@ -167,19 +173,20 @@
         <div class='normalB'> Remaining </div> 
         <div class='normalB'> 75000 </div>
         </div>
-      </div>  
-
-      <center>  <a href='' class='blue-btn d-btn_width'>Donate Now </a> </center>
-
+        </div>
+     <center><a href='' class='blue-btn d-btn_width'>Donate Now </a> </center>
+    
         <div class='colum2'>
-         <div class='subtitleB grey'> Donors </div> <hr> 
-         <img src='<?php echo URL_ROOT; ?>/public/assets/img/welfare_img/image 3.png'>
-       </div>
+         <div class='subtitleB grey'> Donors </div> <hr><br>
+         <div class='normal'>John doe </div><br>
+         <div class='normalB'> 2000lkr </div>
+         <hr width='25%'><br>
+         <div class='normal'>sarah </div><br>
+         <div class='normalB'> 12000lkr </div>
+         <hr width='25%'>   
+        </div>
     </div>
 </div>";}?>
-</div>
-</div>
-
 
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 

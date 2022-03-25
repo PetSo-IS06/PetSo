3d28e65976712f0c4ab932d9c558de560cfe038c
Pasindu Rupasinghe <77311602+pasindur99@users.noreply.github.com>
2022-03-25 09:53:15 +0530

    Implement view all animal profile page (#134)
    * Implement view all animal profile page
    
    * Minor changes
    
    * Some logic implement

--- app/models/AnimalProfile.php
+++ app/models/AnimalProfile.php
@@ -1,43 +1,54 @@
 <?php
 
-    class AnimalProfile {
-        private $db;
+class AnimalProfile
+{
+    private $db;
 
-        public function __construct()
-        {
-            $this->db = new Database;
-        }
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
 
-        public function createProfile($data) {
-            $this->db->query('INSERT INTO `petso`.`Animal_Profile` (`org_id`, `name`, `type`, `age`, `breed`, `gender`, `description`, `image`, `adoption_status`, `requirements`, `sponsorship_status`, `monthly_cost`, `create_date`) 
+    public function createProfile($data)
+    {
+        $this->db->query('INSERT INTO `petso`.`Animal_Profile` (`org_id`, `name`, `type`, `age`, `breed`, `gender`, `description`, `image`, `adoption_status`, `requirements`, `sponsorship_status`, `monthly_cost`, `create_date`) 
             VALUES (:org_id, :name, :type, :age, :breed, :gender, :description, :image, :adoption_status, :requirements, :sponsorship_status, :monthly_cost, :create_date)');
-            
-            $this->db->bind(':org_id', $_SESSION['user_id']);
-            $this->db->bind(':name', $data['name']);
-            $this->db->bind(':type', $data['type']);
-            $this->db->bind(':age', $data['age']);
-            $this->db->bind(':breed', $data['breed']);
-            $this->db->bind(':gender', $data['gender']);
-            $this->db->bind(':description', $data['description']);
-            $this->db->bind(':image', $data['prof-image']);
-            if($data['adoption'] == 'true') {
-                $this->db->bind(':adoption_status', 'Available');
-            }else {
-                $this->db->bind(':adoption_status', 'Not Available');
-            }
-            $this->db->bind(':requirements', $data['requirements']);
-            if($data['sponsorship'] == 'true') {
-                $this->db->bind(':sponsorship_status', 'Available');
-            }else {
-                $this->db->bind(':sponsorship_status', 'Not Available');
-            }
-            $this->db->bind(':monthly_cost', $data['monthlyCost']);
-            $this->db->bind(':create_date', $data['create-date']);
 
-            if($this->db->execute()) {
-                return true;
-            } else {
-                return false;
-            }
+        $this->db->bind(':org_id', $_SESSION['user_id']);
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':type', $data['type']);
+        $this->db->bind(':age', $data['age']);
+        $this->db->bind(':breed', $data['breed']);
+        $this->db->bind(':gender', $data['gender']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':image', $data['prof-image']);
+        if ($data['adoption'] == 'true') {
+            $this->db->bind(':adoption_status', 'Available');
+        } else {
+            $this->db->bind(':adoption_status', 'Not Available');
+        }
+        $this->db->bind(':requirements', $data['requirements']);
+        if ($data['sponsorship'] == 'true') {
+            $this->db->bind(':sponsorship_status', 'Available');
+        } else {
+            $this->db->bind(':sponsorship_status', 'Not Available');
+        }
+        $this->db->bind(':monthly_cost', $data['monthlyCost']);
+        $this->db->bind(':create_date', $data['create-date']);
+
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public function getAllAnimalProfile()
+    {
+        $this->db->query('SELECT P.*, O.org_name FROM `petso`.`Animal_Profile` AS P , `petso`.`Organization` AS O WHERE  O.org_id = P.org_id');
+
+        $result = $this->db->resultSet();    // resultSet returns an array of Objects
+        return $result;
+    }
+
+}
